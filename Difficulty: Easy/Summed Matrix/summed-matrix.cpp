1b46087253da29af5bb2f7cd18e7ee86a0883478
Ashutosh2110 <100460374+Ashutosh2110@users.noreply.github.com>
2024-06-24 13:30:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+     long long sumMatrix(long long n, long long q) {
+        // code here
+        return max((long long)0,(q-1)-max((long long)0,(q-(n+1))*2));
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
